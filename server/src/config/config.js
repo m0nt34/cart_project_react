1f22221b735097d6536eee9b407cd3d6e301e6c8
m0nt34 <116570394+m0nt34@users.noreply.github.com>
2024-11-12 23:09:51 +0400

    cors origin update

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -1,15 +1,17 @@
-
-import cors from 'cors';
-import express from 'express';
+import cors from "cors";
+import express from "express";
 
 const configureApp = (app) => {
   app.use(
     cors({
-      origin: 'http://localhost:5173',
+      origin: [
+        "http://localhost:5173",
+        "https://cart-project-mern.netlify.app",
+      ],
       credentials: false,
     })
   );
-  app.use(express.json()); 
+  app.use(express.json());
 };
 
 export default configureApp;
