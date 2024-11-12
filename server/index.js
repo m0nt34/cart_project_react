4e04f0d57f45e34f647eb7e0fea511cc14ce8037
m0nt34 <116570394+m0nt34@users.noreply.github.com>
2024-11-12 22:08:26 +0400

    backend update for render

--- server/index.js
+++ server/index.js
@@ -1,21 +1,30 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import mongoose from 'mongoose';
-import config from './src/config/config.js'
-import apiRoutes from './src/api/api.js'
-import routes from './src/route/route.js'
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import configureApp from "./src/config/config.js";
+import apiRoutes from "./src/api/api.js";
+import routes from "./src/route/route.js";
 const app = express();
 
-dotenv.config()
+dotenv.config();
 
-app.use(config)
-app.use("/get",apiRoutes)
-app.use("/",routes)
-mongoose.connect(process.env.MONGO_URL).then(()=>{
-  console.log("data base connected")
-  app.listen(process.env.PORT,()=>{
-    console.log("listening on port",process.env.PORT);
+configureApp(app);
+
+app.use("/get", apiRoutes);
+app.use("/", routes);
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   })
-}).catch(err=>{
-  console.log(err)
-})
+  .then(() => {
+    console.log("Database connected");
+
+    // Start the server after successful database connection
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Database connection error:", err);
+  });
--- server/src/config/config.js
+++ server/src/config/config.js
@@ -1,13 +1,15 @@
-import express from 'express'
-import cors from 'cors'
 
-const router = express.Router();
-router.use(
-  cors({
-    origin:'http://localhost:5173',
-    credentials:false,
-  })
-)
-router.use(express.json())
+import cors from 'cors';
+import express from 'express';
 
-export default router
+const configureApp = (app) => {
+  app.use(
+    cors({
+      origin: 'http://localhost:5173',
+      credentials: false,
+    })
+  );
+  app.use(express.json()); 
+};
+
+export default configureApp;
